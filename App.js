e49c5ecaa22c2160d9292e0b9b82beb7c0c13d84
TommiVLavi <bgr912@myy.haaga-helia.fi>
2022-10-22 00:59:06 +0300

    Teht 13 suoritettu

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Button } from 'react-native';
+import * as Contacts from 'expo-contacts'
+import { useState } from 'react';
 
 export default function App() {
+
+  const [contact, setContact] = useState({});
+  const [list, setList] = useState([]);
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync(
+        { fields: [Contacts.Fields.PhoneNumbers] }
+      );
+
+      setList(data)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.table}>
+        <FlatList
+          data={list}
+          keyExtractor={ item => item.lookupKey}
+          renderItem={({ item }) => {
+            const { name, phoneNumbers } = item;
+            return (<Text>{`${name}     ${phoneNumbers ? phoneNumbers[0].number : '(no phone number)'}`}</Text>)
+          }}
+          ListEmptyComponent={<Text>Get some contacts!</Text>}
+        />
+      </View>
+      
+      <View style={styles.button}>
+        <Button title="Hae" onPress={getContacts} />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +50,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  table: {
+    height: '70%'
+  },
+  button: {
+    margin: 20
+  }
 });
